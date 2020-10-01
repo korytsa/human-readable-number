891ba35693396431b1d35942034387490c6d614a
korytsa <anyakorytko@mail.ru>
2020-10-01 13:33:28 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+let first = ['', 'one ', 'two ', 'three ', 'four ','five ', 'six ', "seven ", 'eight ',
+        'nine ', 'ten ', 'eleven ', 'twelve ','thirteen ', 'fourteen ', 'fifteen ',
+        'sixteen ', 'seventeen ', 'eighteen ','nineteen '];
+    
+let second = ['', '', 'twenty ', 'thirty ', 'forty ','fifty ',
+                'sixty ', 'seventy ', 'eighty ','ninety '];
+
 module.exports = function toReadable (number) {
-  
+    if (number != '') {
+        let a = '';
+        a += toWords((parseInt((number / 100) % 10)), "hundred ");
+        a += toWords(parseInt((number % 100)), "");
+        return a.substr(0, a.length - 1);
+    } else{
+        return 'zero';
+    }
+}
+
+function toWords(number, res) {
+    let a = '';
+    if (number > 19)
+        a += second[parseInt(number / 10)] + first[number % 10];
+    else
+        a += first[number];
+    if (number)
+        a += res;
+
+    return a;
 }
